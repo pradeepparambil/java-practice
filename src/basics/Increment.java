6282b2df4913325be665fc9c3bfc79fd2fe981f0
pradeep.pp <pradeep.pp@gmail.com>
2024-03-17 22:28:24 -0400

    Initial commit

---
+++ src/basics/Increment.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Increment {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a++;
+        int c = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
